5d65a43b1ab5bd00ddd5f132709c162d370f843c
btan-fluxus <56370618+btan-fluxus@users.noreply.github.com>
2020-12-30 18:12:28 -0800

    wiki/title display the right content

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -11,8 +12,20 @@ def index(request):
 
 def wiki(request, title):
     listEntries = util.list_entries()
+    content = util.get_entry(title)
+    
     if title.upper() not in map(lambda x:x.upper(), listEntries):
-        return HttpResponse("Title do not exist")
+        message = "The page \"" + title.upper() + "\" does not exist."
+        return render(request, "encyclopedia/error.html", {
+            "message": message
+        })
     else:
-        return HttpResponse(title)
+        
+        #print(content)
+        content = markdown2.markdown(content)
+        #print(content)
+        return render(request, "encyclopedia/content.html", {
+            "title": title,
+            "content": content,
+        })
 
