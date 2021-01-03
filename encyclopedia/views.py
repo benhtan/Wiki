b8ce738f7d85934e1ff0ce1fc8a8afa92b472178
btan-fluxus <56370618+btan-fluxus@users.noreply.github.com>
2021-01-03 14:43:27 -0800

    New Page exit but not functional

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 
 from . import util
 import markdown2
@@ -35,20 +36,46 @@ def content(request, title):
 
 def search(request):
     if request.method == "POST":
+
+        # Get user input in the search box
         q = request.POST['q']
+
+        # See if what user types in matches any .md entries
         content = util.get_entry(q)
+
+        # Making an empty list
         results = []
 
+        # If no matching between search query and .md entries
         if content == None:
+
+            # store list of entries in a list
             entries = util.list_entries()
+
+            # loop over the entries list and check if the search query 
+            # is a subset of any entry. Append to results list
+            # if search query is a subset
             for entry in entries:
                 if q.lower() in entry.lower():
                     results.append(entry)
 
+            # render the html passing through the results list
             return render(request, "encyclopedia/searchResult.html", {
                 "results": results,
             })
         else:
+            # if search query matches one of .db entries than go directly to
+            # the .db entries page
             return HttpResponseRedirect("/wiki/" + q)
+    # if this is a get request than go to index
     return HttpResponseRedirect(reverse("index"))
+
+# Create a class of django forms
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+
+def new(request):
+    return render(request, "encyclopedia/newPage.html", {
+        "form": NewPageForm()
+    })
 
